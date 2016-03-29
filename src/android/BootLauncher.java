6335ee276c610c720fe79799b4fcefffa9c5c2e5
niel <killemall99@gmail.com>
2016-03-29 14:24:10 -0500

    update force app from background to foreground-

--- src/android/BootLauncher.java
+++ src/android/BootLauncher.java
@@ -12,4 +12,16 @@ public class BootLauncher extends BroadcastReceiver {
         serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         context.startActivity(serviceIntent);
     }
+
+    @Override
+    protected void onPause(Context context, Intent intent)
+    {
+      super.onPause();
+      Intent mStartActivity = new Intent(context, com.ionicframework.ohmifismartsync137505.MainActivity.class);
+      int mPendingIntentId = 123456;
+      mStartActivity.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+      PendingIntent mPendingIntent = PendingIntent.getActivity(context, mPendingIntentId, mStartActivity, PendingIntent.FLAG_CANCEL_CURRENT);
+      AlarmManager mgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+      mgr.set(AlarmManager.RTC, System.currentTimeMillis() + 10000, mPendingIntent);
+   }
 }
